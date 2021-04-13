b78944439bbaf444316882ceb07791d05fcec3a7
olga gomelskiy <olichka23.85@gmail.com>
2021-04-13 04:11:47 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,16 @@
-def hello_t
 
-end
+  def hello_t(array)
+     if block_given? #check if there is a block in calling method
+        i = 0
+     while i < array.length
+        yield array[i]
+        i += 1 #looping through each index
+     end
+     array #return the final array
+     else
+        puts "Hey! No block was given!"
+     end
+  end
 
-# call your method here!
-
+# hello_tcall your method here!
+hello_t
